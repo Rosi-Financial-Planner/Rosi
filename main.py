cecb3ffa4418589d68490ac1d7d72409f07b0e49
Daniel Poag <danielpoag@protonmail.com>
2019-11-09 17:48:13 -0500

    Double checking main restore

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-# food, electricity, water, gas
-
-import requests
-import json
-
-customerId = '5dc715db322fa016762f3647'
-apiKey = '226e5d0e1569a36825aafdd02fafb76e'
-
-url = 'http://api.reimaginebanking.com/customers/{}/accounts?key={}'.format(customerId,apiKey)
-
-json = requests.get(url).json()
-
-account_id = json[0]['_id']
-print(account_id)
-purchases_url = 'http://api.reimaginebanking.com/accounts/{}/purchases?key={}'.format(account_id, apiKey)
-=======
 import requests
 import datetime
 
@@ -131,4 +114,3 @@ for account in accounts.keys():
                     )
 
 print(events)
->>>>>>> ec7093990f5f36e8e0315bc453056d2851f5cb55
