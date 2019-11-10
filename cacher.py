2a5c91d3ac990ab4d7939a003cf27db55856bb40
Daniel Poag <danielpoag@protonmail.com>
2019-11-10 01:07:29 -0500

    more attempts to get caching working

--- cacher.py
+++ cacher.py
@@ -1,10 +1,12 @@
 import json
 import apiScrape
 import time
+import os
 
 custId = '5dc6e809322fa016762f363f'
 key = '32b4c33d3c73bb71a1116bba8c3df39e'
 
+os.mkdir['/tmp']
 while 1:
     scraped = apiScrape.scrapeTheApi(custId, key)
     with open('cache.json', 'w') as fp:
